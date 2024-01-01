f5265cb628d52d3127db7f2c218393c22be771bb
elidacon <elidacon@student.21-school.ru>
2024-01-01 21:13:38 +0500

    Add EX01

---
+++ src/reporting_client_v2.py
@@ -0,0 +1,94 @@
+import argparse
+import grpc
+from proto.spaceship_pb2 import Coordinates
+from proto.spaceship_pb2_grpc import SpaceshipServiceStub
+from typing import ClassVar
+from pydantic import BaseModel, ValidationError, model_validator
+from google.protobuf.json_format import MessageToJson
+
+
+class Officer(BaseModel):
+    first_name: str
+    last_name: str
+    rank: str
+
+
+class Spaceship(BaseModel):
+    alignment: str
+    name: str
+    class_: str
+    length: float
+    crew_size: int
+    armed: bool
+    officers: list[Officer]
+
+    checks: ClassVar = {
+        # class_: (length_range, crew_size_range, can_be_armed, can_be_hostile)
+        'Corvette': [(80, 250), (4, 10), True, True],
+        'Frigate': [(300, 600), (10, 15), True, False],
+        'Cruiser': [(500, 1000), (15, 30), True, True],
+        'Destroyer': [(800, 2000), (50, 80), True, False],
+        'Carrier': [(1000, 4000), (120, 250), False, True],
+        'Dreadnought': [(5000, 20000), (300, 500), True, True]
+    }
+
+    @model_validator(mode='after')
+    def check_spaceship(self):
+        limitations = self.checks[self.class_]
+        if not limitations:
+            raise ValueError(f'Invalid class {self.class_}')
+
+        length_range, crew_size_range, can_be_armed, can_be_hostile = limitations
+
+        if not (length_range[0] <= self.length <= length_range[1]):
+            raise ValueError(f'Incorrect length for {self.class_}')
+        elif not (crew_size_range[0] <= self.crew_size <= crew_size_range[1]):
+            raise ValueError(f'Incorrect crew size for {self.class_}')
+        elif (not can_be_armed and self.armed):
+            raise ValueError(f'{self.class_} can not be armed')
+        elif (not can_be_hostile and self.alignment == 'Enemy'):
+            raise ValueError(f'{self.class_} can not be hostile')
+        elif (self.name == 'Unknown' and self.alignment == 'Ally'):
+            raise ValueError(f'Name \'Unknown\' can be only for enemy ships')
+
+        return self
+
+
+# def is_valid_spaceship(data):
+def validate_spaceship(spaceship):
+    try:
+        print(Spaceship.model_validate_json(MessageToJson(
+            spaceship, preserving_proto_field_name=True)).model_dump_json(indent=4))
+    except ValidationError as ve:
+        # print(ve.errors())
+        pass
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description='Spaceship scanner')
+    parser.add_argument(
+        "coordinates",
+        type=float,
+        nargs="*",
+        help="Set of coordinates to scan."
+    )
+    return parser.parse_args().coordinates
+
+
+def run(coordinates):
+    with grpc.insecure_channel('localhost:50051') as channel:
+        # Check if the server is running
+        try:
+            grpc.channel_ready_future(channel).result(timeout=2)
+        except grpc.FutureTimeoutError:
+            print('Error: Server is not running.')
+            return
+
+        stub = SpaceshipServiceStub(channel)
+        for spaceship in stub.GetSpaceships(Coordinates(coordinate=coordinates)):
+            validate_spaceship(spaceship)
+
+
+if __name__ == '__main__':
+    run(parse_args())
