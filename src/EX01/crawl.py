4db288c796e4e1cc9961b4f98187b3032de7a19b
elidacon <elidacon@student.21-school.ru>
2024-01-05 15:20:47 +0500

    Add EX01

---
+++ src/EX01/crawl.py
@@ -0,0 +1,29 @@
+import aiohttp
+import asyncio
+import sys
+
+URL = 'http://localhost:8888/api/v1/tasks/'
+
+
+async def main(urls):
+    async with aiohttp.ClientSession() as session:
+        async with session.post(URL, json={"urls": urls}) as response:
+            task = await response.json()
+        i = 0
+        dots = ['.  ', '.. ', '...']  # dots indicating progress
+        delay = max(0.5, min(len(urls)/100, 2))  # delay min 0.5, max 2 seconds
+        while True:
+            async with session.get(f'{URL}{task["id"]}') as response:
+                data = await response.json()
+            if data.get("status") == "ready":
+                print('\rResults:   ')
+                for url, status in data.get("result", {}).items():
+                    print(f"{status}\t{url}")
+                break
+            print('Checking' + dots[i % 3], end='\r')
+            i += 1
+            await asyncio.sleep(delay)
+
+if __name__ == '__main__':
+    urls = sys.argv[1:]
+    asyncio.run(main(urls))
---
+++ src/EX01/server.py
@@ -0,0 +1,62 @@
+from fastapi import FastAPI, HTTPException, BackgroundTasks
+from pydantic import BaseModel, HttpUrl, Field
+from uuid import uuid4, UUID
+from typing import List, Dict, Literal
+from aiohttp import ClientSession, ClientTimeout
+import asyncio
+import uvicorn
+
+
+class Task(BaseModel):
+    id: UUID
+    status: Literal["running", "ready"]
+    result: Dict[HttpUrl, int] = Field(default_factory=dict)
+
+
+class Request(BaseModel):
+    urls: List[HttpUrl]
+
+
+app = FastAPI()
+tasks: Dict[UUID, Task] = {}
+
+
+async def fetch_url(session, url, task_id):
+    try:
+        async with session.get(str(url), timeout=ClientTimeout(total=5)) as response:
+            tasks[task_id]["result"][url] = response.status
+    except Exception as e:
+        print(f"Error fetching {url}: {str(e)}")
+        tasks[task_id]["result"][url] = -1
+
+
+async def fetch_all_urls(urls: List[HttpUrl], task_id: UUID):
+    async with ClientSession() as session:
+        workers = []
+        for url in urls:
+            workers.append(asyncio.ensure_future(
+                fetch_url(session, url, task_id)))
+        await asyncio.gather(*workers)
+
+    tasks[task_id]["status"] = "ready"
+
+
+@app.post("/api/v1/tasks/", response_model=Task, status_code=201)
+async def create_task(request: Request, background_tasks: BackgroundTasks):
+    task_id = uuid4()
+    task = Task(id=task_id, status="running")
+    tasks[task_id] = task.model_dump()
+    background_tasks.add_task(fetch_all_urls, request.urls, task_id)
+
+    return task
+
+
+@app.get("/api/v1/tasks/{task_id}", response_model=Task)
+async def read_task(task_id: UUID):
+    task = tasks.get(task_id)
+    if not task:
+        raise HTTPException(status_code=404, detail="Task not found")
+    return task
+
+if __name__ == "__main__":
+    uvicorn.run(app, port=8888)
