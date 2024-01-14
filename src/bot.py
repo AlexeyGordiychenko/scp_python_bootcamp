26c5076c31ef69d0b6cf6c1367d5f388f83ac6b5
elidacon <elidacon@student.21-school.ru>
2024-01-14 19:41:19 +0500

    Refactor bot.py to save character in the state (FSMContext)

--- src/bot.py
+++ src/bot.py
@@ -16,6 +16,7 @@ import db
 import kb
 import msg_text
 from sqlalchemy import select
+from sqlalchemy.orm import selectinload
 
 load_dotenv()
 TOKEN = getenv('TOKEN')
@@ -30,13 +31,16 @@ class MenuStates(StatesGroup):
 
 
 @router.message(Command("start"))
-async def start_command(message: Message):
+async def start_command(message: Message, state: FSMContext):
     with db.Session() as session:
-        existing_character = session.execute(select(db.Character.name).where(
-            db.Character.id == message.from_user.id)).scalars().first()
-
+        existing_character = session.execute(
+            select(db.Character)
+            .options(selectinload(db.Character.location), selectinload(db.Character.items))
+            .where(db.Character.id == message.from_user.id)
+        ).scalar_one_or_none()
     if existing_character:
-        await message.answer(msg_text.welcome.format(name=existing_character), reply_markup=kb.main_menu)
+        await state.update_data(character=existing_character)
+        await message.answer(msg_text.welcome.format(name=existing_character.name), reply_markup=kb.main_menu)
     else:
         await message.answer(msg_text.welcome_new, reply_markup=kb.create_character_menu)
 
@@ -52,124 +56,125 @@ async def input_character_name(clbck: CallbackQuery, state: FSMContext):
     await clbck.message.answer(msg_text.enter_name)
 
 
-@router.callback_query(F.data == "get_location")
-async def get_location(callback_query: CallbackQuery):
+@router.message(MenuStates.create_character)
+async def create_character(message: Message, state: FSMContext):
+    name = message.text
+    new_character = db.Character(id=message.from_user.id, name=name)
     with db.Session() as session:
-        current_location = session.execute(select(db.Location.name).join(db.Character).where(
-            db.Character.id == callback_query.from_user.id)).scalars().first()
+        session.add(new_character)
+        session.commit()
+        session.refresh(new_character, attribute_names=['location', 'items'])
+        await state.update_data(character=new_character)
+    await message.answer(msg_text.create_succ, reply_markup=kb.main_menu)
 
-    if not current_location:
-        current_location = 'Unknown'
-    # await bot.send_message(callback_query.from_user.id, msg_text.current_location.format(location=current_location), reply_markup=kb.main_menu)
-    await callback_query.message.edit_text(msg_text.current_location.format(location=current_location), reply_markup=kb.main_menu)
+
+@router.callback_query(F.data == "get_location")
+async def get_location(callback_query: CallbackQuery, state: FSMContext):
+    data = await state.get_data()
+    character = data.get('character')
+    await callback_query.message.edit_text(msg_text.current_location.format(location=character.location.name), reply_markup=kb.main_menu)
 
 
 @router.callback_query(F.data == "get_hp")
-async def get_hp(callback_query: CallbackQuery):
-    with db.Session() as session:
-        current_health = session.execute(select(db.Character.hp).where(
-            db.Character.id == callback_query.from_user.id)).scalars().first()
-    if not current_health:
-        current_health = 'Unknown'
-    # await bot.send_message(callback_query.from_user.id, msg_text.current_health.format(health=current_health), reply_markup=kb.main_menu)
-    await callback_query.message.edit_text(msg_text.current_health.format(health=current_health), reply_markup=kb.main_menu)
+async def get_hp(callback_query: CallbackQuery, state: FSMContext):
+    data = await state.get_data()
+    character = data.get('character')
+    await callback_query.message.edit_text(msg_text.current_health.format(health=character.hp), reply_markup=kb.main_menu)
 
 
 @router.callback_query(F.data == "get_inventory")
-async def get_inventory(callback_query: CallbackQuery):
-    with db.Session() as session:
-        current_character = session.execute(select(db.Character).where(
-            db.Character.id == callback_query.from_user.id)).scalars().first()
-        inventory_msg = '\n'.join(
-            [f'{item.item}: {item.count}' for item in current_character.items])
-        # await bot.send_message(callback_query.from_user.id, f'{msg_text.current_inventory}\n{inventory_msg}', reply_markup=kb.main_menu)
+async def get_inventory(callback_query: CallbackQuery, state: FSMContext):
+    data = await state.get_data()
+    character = data.get('character')
+    inventory_msg = '\n'.join(
+        [f'{item.item}: {item.count}' for item in character.items])
     await callback_query.message.edit_text(f'{msg_text.current_inventory}{inventory_msg}', reply_markup=kb.main_menu)
 
 
 @router.callback_query(F.data == "change_location")
-async def change_location(callback_query: CallbackQuery):
+async def change_location(callback_query: CallbackQuery, state: FSMContext):
+    data = await state.get_data()
+    character = data.get('character')
     with db.Session() as session:
-        current_location = session.execute(select(db.Location).join(db.Character).where(
-            db.Character.id == callback_query.from_user.id)).scalars().first()
-        linked_locations = current_location.linked_locations
+        session.add(character)
+        linked_locations = character.location.linked_locations
     builder = InlineKeyboardBuilder()
     for location in linked_locations:
         builder.button(text=location.name,
-                       callback_data=f'set_location:{location.id}:{location.name}:{location.description}')
+                       callback_data=f'set_location:{location.id}')
     builder.button(text=msg_text.back, callback_data='main_menu')
     builder.adjust(2)
 
-    # await bot.send_message(callback_query.from_user.id, msg_text.change_location_ask, reply_markup=builder.as_markup())
     await callback_query.message.edit_text(msg_text.change_location_ask, reply_markup=builder.as_markup())
 
 
 @router.callback_query(F.data.startswith("set_location:"))
-async def set_location(callback_query: CallbackQuery):
-    _, location_id, location_name, location_desc = callback_query.data.split(
-        ':')
+async def set_location(callback_query: CallbackQuery, state: FSMContext):
+    data = await state.get_data()
+    character = data.get('character')
+    _, location_id = callback_query.data.split(':')
     with db.Session() as session:
-        character = session.execute(select(db.Character).where(
-            db.Character.id == callback_query.from_user.id)).scalars().first()
-        if character:
-            character.location_id = location_id
-            session.commit()
-
-    # await bot.send_message(callback_query.from_user.id, msg_text.change_location_succ.format(location=location_name), reply_markup=kb.main_menu)
-    await callback_query.message.edit_text(msg_text.change_location_succ.format(location=location_name, desc=location_desc), reply_markup=kb.main_menu)
+        session.add(character)
+        character.location_id = int(location_id)
+        session.commit()
+        session.refresh(character, attribute_names=['location', 'items'])
+    await state.update_data(character=character)
+    await callback_query.message.edit_text(msg_text.change_location_succ.format(location=character.location.name, desc=character.location.description), reply_markup=kb.main_menu)
 
 
 @router.callback_query(F.data == "get_npcs")
-async def get_npcs(callback_query: CallbackQuery):
+async def get_npcs(callback_query: CallbackQuery, state: FSMContext):
+    data = await state.get_data()
+    character = data.get('character')
+
     with db.Session() as session:
-        current_location = session.execute(select(db.Location).join(db.Character).where(
-            db.Character.id == callback_query.from_user.id)).scalars().first()
-        npcs = current_location.npcs
+        session.add(character)
+        npcs = character.location.npcs
     if npcs:
         builder = InlineKeyboardBuilder()
         for npc in npcs:
             builder.button(text=npc.name,
-                           callback_data=f'talk_to_npc:{npc.id}:{npc.name}:{npc.dialog_id}:1')
+                           #    callback_data=f'talk_to_npc:{npc.id}:{npc.name}:{npc.dialog_id}:1')
+                           callback_data=f'talk_to_npc:{npc.id}:{npc.name}:1')
         builder.button(text=msg_text.back, callback_data='main_menu')
         builder.adjust(1)
 
-        # await bot.send_message(callback_query.from_user.id, msg_text.pick_npc, reply_markup=builder.as_markup())
         await callback_query.message.edit_text(msg_text.pick_npc, reply_markup=builder.as_markup())
     else:
         await callback_query.message.edit_text(msg_text.no_npcs_in_location, reply_markup=kb.main_menu)
 
 
 @router.callback_query(F.data.startswith("talk_to_npc:"))
-async def talk_to_npc(callback_query: CallbackQuery):
-    _, npc_id, npc_name, dialog_id, stage = callback_query.data.split(':')
+async def talk_to_npc(callback_query: CallbackQuery, state: FSMContext):
+    _, npc_id, npc_name, stage_id = callback_query.data.split(':')
     with db.Session() as session:
-        npc_text = session.execute(select(db.Dialog.npc_text).where(
-            db.Dialog.id == dialog_id, db.Dialog.stage == stage)).scalars().first()
-        responses = session.execute(select(db.PlayerResponse).where(
-            db.PlayerResponse.dialog_id == dialog_id, db.PlayerResponse.stage_id == stage)).scalars().all()
+        dialog = session.execute(
+            select(db.Dialog)
+            .where(db.Dialog.npc_id == npc_id, db.Dialog.stage_id == stage_id)
+        ).scalar_one()
+        responses = dialog.responses
     builder = InlineKeyboardBuilder()
     for response in responses:
         if response.next_stage_id:
             builder.button(text=response.text,
-                           callback_data=f'talk_to_npc:{npc_id}:{npc_name}:{dialog_id}:{response.next_stage_id}')
+                           callback_data=f'talk_to_npc:{npc_id}:{npc_name}:{response.next_stage_id}')
         else:
             builder.button(text=response.text,
                            callback_data=f'leave_npc:{npc_name}')
     builder.adjust(1)
 
-    # await bot.send_message(callback_query.from_user.id, npc_text, reply_markup=builder.as_markup())
-    await callback_query.message.edit_text(npc_text, reply_markup=builder.as_markup())
+    await callback_query.message.edit_text(dialog.npc_text, reply_markup=builder.as_markup())
 
 
 @router.callback_query(F.data.startswith("leave_npc:"))
 async def leave_npc(callback_query: CallbackQuery):
     npc_name = callback_query.data.split(':')[1]
-    # await bot.send_message(callback_query.from_user.id, msg_text.leave_npc.format(name=npc_name), reply_markup=kb.main_menu)
     await callback_query.message.edit_text(msg_text.leave_npc.format(name=npc_name), reply_markup=kb.main_menu)
 
 
 @router.callback_query(F.data == "get_enemies")
-async def get_enemies(callback_query: CallbackQuery):
-    buttons = await get_buttons_for_enemies(callback_query.from_user.id)
+async def get_enemies(callback_query: CallbackQuery, state: FSMContext):
+    buttons = await get_buttons_for_enemies(state)
     if buttons:
         await callback_query.message.edit_text(msg_text.pick_enemy, reply_markup=buttons)
     else:
@@ -177,55 +182,53 @@ async def get_enemies(callback_query: CallbackQuery):
 
 
 @router.callback_query(F.data.startswith("fight:"))
-async def fight(callback_query: CallbackQuery):
+async def fight(callback_query: CallbackQuery, state: FSMContext):
     _, enemy_id, enemy_name, enemy_level, enemy_loot = callback_query.data.split(
         ':')
-    with db.Session() as session:
-        character = session.execute(select(db.Character).where(
-            db.Character.id == callback_query.from_user.id)).scalars().first()
+    data = await state.get_data()
+    character = data.get('character')
 
-        character_total = randint(1, 6) + int(character.level)
-        enemy_total = randint(1, 6) + int(enemy_level)
+    character_total = randint(1, 6) + int(character.level)
+    enemy_total = randint(1, 6) + int(enemy_level)
 
-        if character_total >= enemy_total:
-            character.level += 1
-            if enemy_loot:
-                item = session.execute(select(db.Inventory).where(
-                    db.Inventory.character_id == callback_query.from_user.id, db.Inventory.item == enemy_loot)).scalars().first()
-                if item:
-                    item.count += 1
-                else:
-                    session.add(db.Inventory(
-                        character_id=callback_query.from_user.id, item=enemy_loot, count=1))
-            result_text = msg_text.fight_succ.format(
-                enemy=enemy_name, level=character.level, loot=enemy_loot)
-        else:
-            character.hp -= 1
-            result_text = msg_text.fight_fail.format(
-                enemy=enemy_name, hp=character.hp)
-        session.commit()
-    buttons = await get_buttons_for_enemies(callback_query.from_user.id)
+    if character_total >= enemy_total:
+        character.level += 1
+        if enemy_loot:
+
+            existing_item = next(
+                (item for item in character.items if item.item == enemy_loot), None)
+            if existing_item:
+                existing_item.count += 1
+            else:
+                new_item = db.Inventory(
+                    character_id=character.id, item=enemy_loot, count=1)
+                character.items.append(new_item)
+
+            with db.Session() as session:
+                session.add(character)
+                session.commit()
+                session.refresh(character, attribute_names=[
+                                'location', 'items'])
+            await state.update_data(character=character)
+        result_text = msg_text.fight_succ.format(
+            enemy=enemy_name, level=character.level, loot=enemy_loot)
+    else:
+        character.hp -= 1
+        result_text = msg_text.fight_fail.format(
+            enemy=enemy_name, hp=character.hp)
+    buttons = await get_buttons_for_enemies(state)
     await callback_query.message.edit_text(f"{result_text}\n{msg_text.pick_enemy}", reply_markup=buttons)
 
 
-@router.message(MenuStates.create_character)
-async def process_name(message: Message, state: FSMContext):
-    name = message.text
-    new_character = db.Character(id=message.from_user.id, name=name)
-    with db.Session() as session:
-        session.add(new_character)
-        session.commit()
-    await message.answer(msg_text.create_succ, reply_markup=kb.main_menu)
-    # await message.edit_text(msg_text.create_succ, reply_markup=kb.main_menu)
+async def get_buttons_for_enemies(state: FSMContext) -> InlineKeyboardMarkup:
+    data = await state.get_data()
+    character = data.get('character')
 
-
-async def get_buttons_for_enemies(user_id: int) -> InlineKeyboardMarkup:
     with db.Session() as session:
-        current_location = session.execute(select(db.Location).join(db.Character).where(
-            db.Character.id == user_id)).scalars().first()
-        enemies = current_location.enemies
+        session.add(character)
+        enemies = character.location.enemies
+
     if enemies:
-        print('enemies found')
         builder = InlineKeyboardBuilder()
         for enemy in enemies:
             builder.button(text=enemy.name,
